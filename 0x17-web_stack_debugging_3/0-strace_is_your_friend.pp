445060cd0862dd44291d88e1b2f5d2aeeac74ff9
Daniel Eze <edycram4u@gmail.com>
2023-05-09 13:11:43 -0700

    web stack debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,10 +1,6 @@
-# A puppet manuscript to replace a line in a file on a server
+# fix wordpress 500 internal server error
 
-$file_to_edit = '/var/www/html/wp-settings.php'
-
-#replace line containing "phpp" with "php"
-
-exec { 'replace_line':
-  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
-  path    => ['/bin','/usr/bin']
-}
+exec { 'fix-wordpress':
+  command => 'cp /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
+  path    => '/bin/:/usr/bin/:/usr/local/bin/:/usr/sbin/'
+} # Note sed command can be used: "sudo sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php"
